96b86acd3685d12231edf9b471f75b437c7e95c2
ternprojects <ternxstyle@gmail.com>
2021-10-30 03:36:26 +0300

    added_project_files

---
+++ build/js/scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var modal = document.getElementById('myModal');
+var span = document.getElementById('close');
+
+function showModal() {
+  modal.style.display = 'flex';
+}
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  console.log(event.target);
+
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
